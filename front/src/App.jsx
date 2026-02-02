e7a6a66671bfd4ed8bd3f97f90473b4341c4b5f6
AdiWanderer <darpentevdenis11@gmail.com>
2026-02-02 04:11:46 -0800

    frontend basics thing added.

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const API_URL = "http://localhost:3000/api/ToDos";
+
+
+
+export default function App() {
+  const [ToDos, setToDo] = useState([]);
+
+  const fetchToDo = async () => {
+  try {
+    const res = await axios.get(API_URL);
+    setToDo(res.data);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+
+  useEffect(() => {
+    fetchToDo();
+  }, []);
+
+  const AddToDo = () => {
+    console.log("Add clicked");
+  };
+
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div class='title'>
+          <h1>ToDo.com</h1>
+        </div>
+        <div class='add_button'>
+          <button class='add_button' onClick={AddToDo}>Add</button>
+        </div>
+        <div class='container'>
+          <div class='un done boxlist'>
+            <div class='list'>
+              <p>left box</p>
+            </div>
+          </div>
+          <div class='done boxlist'>
+            <div class='list'>
+              <p>right box</p>
+              <ul>
+                {ToDos.map((T) => (
+                  <li key={T.id}>{T.text}{" "}
+                  <button>Edit</button>{" "}
+                  <button>Delete</button></li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
 
-export default App
