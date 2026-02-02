1447390ee10040d744cb1e5a6cb2270bcd2f4bd9
Hyp3rGam3r <mykola.stankov07@gmail.com>
2026-02-02 10:27:18 +0200

    react

---
+++ back/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import express from "express";
+
+
+
+const app = express();
+app.use(express.json());
+
+dotenv.config();
